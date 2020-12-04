8b8203c94eb88514c39f40e96d6e295d93ff2ea1
PARK SUNG DONG <oraedoa@gmail.com>
2020-12-04 23:35:04 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# tasty-food-seeker
+맛있는 음식점을 찾아라
+
+## 개요
+서울시 업무추진비로 이용하는 음식점은 어디어디가 있을까?
+
+## reference
+* [java text similarity](http://www.gisdeveloper.co.kr/?p=9756)
---
+++ tasty-food-seeker-web/src/test/java/org/pasudo123/tastyfoodseeker/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pasudo123.tastyfoodseeker.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
