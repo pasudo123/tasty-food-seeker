2b57ff476cc5ba97aa5fb52e12aa57d21aac0e4c
PARK SUNG DONG <oraedoa@gmail.com>
2020-12-12 15:44:14 +0900

    [feat] pagination

--- tasty-food-seeker-web/frontend/src/components/explore/Explore.vue
+++ tasty-food-seeker-web/frontend/src/components/explore/Explore.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="search">
+  <div id="explore">
     <div class="selectorWrapper">
       <b-input-group>
         <template #prepend>
@@ -7,9 +7,32 @@
         </template>
         <b-form-select v-model="selected" :options="locationOptions"></b-form-select>
         <template #append>
-          <b-button variant="outline-dark" class="exploreBtn" @click="exploreTastyLocation">조회</b-button>
+          <b-overlay class="exploreOverlay" :show="fetchBtnLoading" opacity="0.6" spinner-small>
+            <b-button variant="outline-dark" class="exploreBtn" @click="exploreTastyLocation()">조회</b-button>
+          </b-overlay>
         </template>
       </b-input-group>
+    </div>
+    <div class="tableWrapper">
+      <b-overlay :show="fetchTableLoading" spinner-type="grow">
+        <b-table fixed
+                 id="location-table"
+                 primary-key="id"
+                 :head-variant="'dark'"
+                 :fields="this.tableFields"
+                 :items="this.items"
+                 :per-page="perPage">
+          <template #cell(index)="data">
+            {{ (data.index + 1) + (currentPage - 1) * 10 }}
+          </template>
+        </b-table>
+      </b-overlay>
+      <b-pagination v-model="currentPage"
+                    aria-control="location-table"
+                    align="center"
+                    @change="changeCurrentPage"
+                    :total-rows="rows"
+                    :per-page="perPage"></b-pagination>
     </div>
   </div>
 </template>
@@ -19,13 +42,20 @@
   import {createNamespacedHelpers} from 'vuex'
 
   const {
-    mapActions: exploreMapActions
+    mapActions: exploreMapActions,
+    mapGetters: exploreMapGetters
   } = createNamespacedHelpers('explore')
 
   export default {
-    name: "Search",
+    name: "Explore",
     data() {
       return {
+        items: [],
+        currentPage: 1,
+        perPage: 10,
+        rows: 0,
+        fetchBtnLoading: false,
+        fetchTableLoading: false,
         selected: null,
         locationOptions: [
           {text: '전체', value: 'ALL'},
@@ -57,14 +87,44 @@
         ]
       }
     },
+    computed: {
+      ...exploreMapGetters(['currentLocationItems', 'tableFields'])
+    },
     methods: {
       ...exploreMapActions(['fetchLocationByGu']),
 
       exploreTastyLocation() {
+        this.currentPage = 1;
+
         const params = {}
         params.selected = this.selected;
-        this.fetchLocationByGu(params).then(() => {
+        params.currentPage = this.currentPage;
+        params.size = this.perPage;
 
+        this.fetchBtnLoading = true;
+        this.fetchLocationByGu(params).then(() => {
+          this.fetchBtnLoading = false;
+          this.items = this.currentLocationItems.items;
+          this.rows = this.currentLocationItems.totalElements;
+        }).catch(() => {
+          this.items = [];
+          this.fetchBtnLoading = false;
+        })
+      },
+
+      changeCurrentPage(currentPage) {
+        const params = {}
+
+        this.fetchTableLoading = true;
+        params.selected = this.selected;
+        params.currentPage = currentPage;
+        params.size = this.perPage;
+        this.fetchLocationByGu(params).then(() => {
+          this.items = this.currentLocationItems.items;
+          this.rows = this.currentLocationItems.totalElements;
+          this.fetchTableLoading = false;
+        }).catch(() => {
+          this.fetchTableLoading = false;
         })
       }
     }
@@ -78,8 +138,15 @@
       padding: 0 25px;
     }
 
-    .exploreBtn {
-      padding: 0 25px;
+    .exploreOverlay {
+      .exploreBtn {
+        padding: 0 25px;
+        height: 100%;
+      }
+    }
+
+    .tableWrapper {
+      margin: 15px 0 5px 0;
     }
   }
 </style>
--- tasty-food-seeker-web/frontend/src/store/modules/explore.js
+++ tasty-food-seeker-web/frontend/src/store/modules/explore.js
@@ -1,8 +1,23 @@
 import request from '../../util/request'
 
-const state = {}
+const state = {
+    tableFields: [
+        { key: 'index', label: '#' },
+        { key: 'name', label: '가게명' },
+        { key: 'category', label: '분류'},
+        { key: 'roadAddress', label: '주소' }
+    ],
+    currentLocationItems:{
+        items:[],
+        totalElements: 0,
+        totalPages: 0
+    }
+}
 
-const getters = {}
+const getters = {
+    tableFields: (state) => state.tableFields,
+    currentLocationItems: (state) => state.currentLocationItems
+}
 
 const actions = {
 
@@ -10,19 +25,26 @@ const actions = {
         const uri = `/api/restaurants`
         const queryParam = {}
         queryParam.gu = params.selected;
-        queryParam.page = 1;
-        queryParam.size = 10;
+        queryParam.page = params.currentPage;
+        queryParam.size = params.size;
 
         return new Promise((resolve, reject) => {
             request.get(uri, {params: queryParam}).then((response) => {
-                console.debug(response);
+                commit('setCurrentLocationItems', response);
                 resolve();
-            })
+            }).catch(() => reject())
         })
     }
 }
 
-const mutations = {}
+const mutations = {
+    setCurrentLocationItems(state, data) {
+        state.currentLocationItems.items = [];
+        state.currentLocationItems.items = data.restaurantResponseDtos;
+        state.currentLocationItems.totalElements = data.totalElements;
+        state.currentLocationItems.totalPages = data.totalPages;
+    }
+}
 
 export default {
     namespaced: true,
