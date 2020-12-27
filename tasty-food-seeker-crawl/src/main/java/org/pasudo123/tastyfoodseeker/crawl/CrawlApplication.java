9112f76dc3e8a05264e257adad606d8ab3777ee9
PARK SUNG DONG <oraedoa@gmail.com>
2020-12-27 13:45:21 +0900

    [feat] app runner include arguments

--- tasty-food-seeker-crawl/src/main/java/org/pasudo123/tastyfoodseeker/crawl/CrawlApplication.java
+++ tasty-food-seeker-crawl/src/main/java/org/pasudo123/tastyfoodseeker/crawl/CrawlApplication.java
@@ -1,36 +1,41 @@
 package org.pasudo123.tastyfoodseeker.crawl;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.pasudo123.tastyfoodseeker.crawl.component.TastyFoodSeekExplorer;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Arrays;
 
 @Slf4j
 @SpringBootApplication
-@RequiredArgsConstructor
 @ComponentScan(value = {"org.pasudo123.tastyfoodseeker.*"})
-public class CrawlApplication implements CommandLineRunner {
+public class CrawlApplication {
 
-	private final TastyFoodSeekExplorer explorer;
+	private static final String OPT_YEAR = "crawl.year";
+	private static final String OPT_MONTH = "crawl.month";
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = new SpringApplicationBuilder(CrawlApplication.class)
-				.web(WebApplicationType.NONE).run();
+				.web(WebApplicationType.NONE) 	// web application NONE 타입
+				.run(args);						// command-line args 추가.
+
 		TastyFoodSeekExplorer bean = context.getBean(TastyFoodSeekExplorer.class);
 		bean.closeAndQuitBrowser();
 		context.close();
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("args : {}", Arrays.toString(Arrays.stream(args).toArray()));
-		log.info("====> do crawl");
+	@Bean
+	public ApplicationRunner applicationRunner(final TastyFoodSeekExplorer explorer) {
+		return args -> {
+			final int year = Integer.parseInt(args.getOptionValues(OPT_YEAR).get(0));
+			final int month = Integer.parseInt(args.getOptionValues(OPT_MONTH).get(0));
+			log.info("=============================");
+			log.info(":: crawling date :: {}", String.format("%4d/%2d", year, month));
+			log.info("=============================\n");
+		};
 	}
 }
