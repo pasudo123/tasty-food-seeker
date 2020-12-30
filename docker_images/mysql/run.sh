590f6e65c7a552dd0cd55ec25bbcf20c5cdd2569
PARK SUNG DONG <oraedoa@gmail.com>
2020-12-30 10:11:33 +0900

    [feat] create shell on mysql backup & restore

--- docker_images/mysql/run.sh
+++ docker_images/mysql/run.sh
@@ -1,12 +1,54 @@
 #!/bin/bash
 
-docker build -t tasty-food-seeker-storage .
+RED_COLOR='\e[31m'
+DEFAULT_COLOR='\e[39m'
 
-docker run -d -p 30666:3306 \
--e MYSQL_ROOT_PASSWORD=rootpass \
--e MYSQL_ROOT_HOST=% \
--e MYSQL_DATABASE=tasty-food-seeker \
---name tasty_food_seeker_storage \
-tasty-food-seeker-storage
+rootpwd=rootpass
+database=tasty-food-seeker
+container=tasty-food-seeker-storage
+backupPath='/data/backup/db'
+
+usage() {
+  echo -e "${RED_COLOR}Please write as below.${DEFAULT_COLOR}"
+  echo "./run.sh { build | run | backup | restore }"
+}
+
+build() {
+  docker build -t tasty-food-seeker-storage .
+}
+
+run() {
+  docker run -d -p 30666:3306 \
+          -e MYSQL_ROOT_PASSWORD=${rootpwd} \
+          -e MYSQL_ROOT_HOST=% \
+          -e MYSQL_DATABASE=${database} \
+          --name tasty_food_seeker_storage \
+          ${container}
+}
+
+backup() {
+  docker exec ${container} /usr/bin/mysqldump -u root --password=${rootpwd} ${database} > ${backupPath}/db_backup.sql
+}
+
+restore() {
+  cat ${backupPath}/db_backup.sql | docker exec -i ${container} /usr/bin/mysql -u root --password=${rootpwd} ${database}
+}
 
 
+case $1 in
+build)
+  build
+  ;;
+run)
+  run
+  ;;
+backup)
+  backup
+  ;;
+restore)
+  restore
+  ;;
+*)
+  usage
+  ;;
+esac
