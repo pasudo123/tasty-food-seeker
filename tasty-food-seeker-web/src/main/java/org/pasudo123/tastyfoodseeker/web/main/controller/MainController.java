632711ca256a1d4e3a78aac596e0cf59109d68f5
PARK SUNG DONG <oraedoa@gmail.com>
2021-01-01 14:20:40 +0900

    [feat] web application page request mapping & addtional refactor

--- tasty-food-seeker-web/src/main/java/org/pasudo123/tastyfoodseeker/web/main/controller/MainController.java
+++ tasty-food-seeker-web/src/main/java/org/pasudo123/tastyfoodseeker/web/main/controller/MainController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping
+    @GetMapping(value = {"/",
+            "/explore", "/explore/*"})
     public String main() {
         return "index";
     }
