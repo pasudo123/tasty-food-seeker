511e7169f0003e5bac43c61fe804962492799e4c
PARK SUNG DONG <oraedoa@gmail.com>
2021-01-31 15:23:12 +0900

    [feat] add crawl app github action

--- tasty-food-seeker-crawl/script/run.sh
+++ tasty-food-seeker-crawl/script/run.sh
@@ -6,25 +6,25 @@ CYAN_COLOR='\e[96m'
 GREEN_COLOR='\e[32m'
 DEFAULT_COLOR='\e[39m'
 
-buildDirectory="build/libs/"
-appName="tasty-food-seeker-crawl"
+#buildDirectory="build/libs/"
+#appName="tasty-food-seeker-crawl"
 
-cd ../../
+#cd ../../
 
 #echo -e "\n===> ${GREEN_COLOR}build jar file${DEFAULT_COLOR}"
 #buildJarFile="./gradlew :${appName}:bootJar"
 #$buildJarFile
 
-cd ${appName} || exit
+#cd ${appName} || exit
 
-echo -e "\n===> ${GREEN_COLOR}latest jar file${DEFAULT_COLOR}"
-fetchLatestJarFile=`ls -t "$buildDirectory" | head -1`
-latestJar=$fetchLatestJarFile
-echo -e "\n===> latest jar :: ${RED_COLOR}${latestJar}${DEFAULT_COLOR}"
+#echo -e "\n===> ${GREEN_COLOR}latest jar file${DEFAULT_COLOR}"
+#fetchLatestJarFile=`ls -t "$buildDirectory" | head -1`
+#latestJar=$fetchLatestJarFile
+#echo -e "\n===> latest jar :: ${RED_COLOR}${latestJar}${DEFAULT_COLOR}"
 
 # using docker-compose.yml
-# jar 파일은 하나만 있어야 한다.
-P_YEAR=2020
-P_MONTH=11
-echo -e "\n===> ${CYAN_COLOR}using docker-compose container${DEFAULT_COLOR}"
-LATEST_JAR="$latestJar" YEAR=${P_YEAR} MONTH=${P_MONTH} docker-compose up -d --build
+#P_LATEST_JAR=''
+#P_YEAR=2020
+#P_MONTH=11
+#echo -e "\n===> ${CYAN_COLOR}using docker-compose container${DEFAULT_COLOR}"
+#YEAR=${P_YEAR} MONTH=${P_MONTH} docker-compose up -d --build
